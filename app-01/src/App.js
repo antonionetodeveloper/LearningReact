4b92e2d8c819648a8af4bd82f2a6fe9884136d63
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-05 21:16:28 -0300

    initial commit

---
+++ app-01/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          VAI SE FODER SULISTA
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
