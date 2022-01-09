0c357da7a27239d05018edc04e1f8e396e359da8
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-09 14:51:51 -0300

    update commit

--- app-03/src/App.js
+++ app-03/src/App.js
@@ -28,7 +28,7 @@ function App() {
           <div className="imgAndText">
             <img src={myPhoto} className="img"></img>
             <text className="text">
-              Antônio Fernandes
+              Antônio Fernandes. 
               Programador e desenvolvedor há mais de 3 anos.
               Teve início na carreira de desenvolvimento em 
               2018, quando foi chamado para fazer um curso
@@ -39,7 +39,8 @@ function App() {
               nas diversas linguagens de programação e
               tecnologias hoje utilizadas no mercado.
               <br/><br/>
-              Fundador da ideia <a href="https://site2it.com">Site2it</a>,
+              Fundador da ideia <a href="https://antonionetodeveloper.github.io/Site2it_NEW/"
+              target="_blank">Site2it</a>,
               aplicativo queconecta desenvolvedores a clientes, usando 
               tecnologias do mercado para atender da melhor
               maneira seus clientes, buscando desenvolvedores
@@ -58,20 +59,61 @@ function App() {
               após dia todo o conhecimento que adquire, 
               administrando, também, o trajeto que suas ideias 
               e projetos tomam.
+              <br></br>
+              <br></br>
+              <p className="descricao"><a href="https://wa.me/+5579988105028" target="_blank">Whatsapp: (79) 9 8810-5028</a> / Aracaju - SE</p>
             </text>
           </div>
         </section>
 
         <section id="services">
+          <div className="sites">
+            <img src={myPhoto}/>
+            <div className="subtitle">
+              <h3>Criação de sites</h3>
+              <p>Sites com as tecnologias HTML, <br/>CSS, JavaScript, ReactJS e MySQL</p>
+            </div>
+          </div>
 
+          <div className="apps">
+            <img src={myPhoto}/>
+            <div className="subtitle">
+              <h3>Desenvolvimento de Apps</h3>
+              <p>Para despositivos IOS e ANDROID com<br/> a tecnologia React-Native</p>
+            </div>
+          </div>
+
+          <div className="Ecomerces">
+            <img src={myPhoto}/>
+            <div className="subtitle">
+              <h3>Ecomerce</h3>
+              <p>Sua loja virtual que funciona 24hrs <br/> efetuando compras 100% online</p>
+            </div>
+          </div>
         </section>
 
         <section id="portfolio">
-
+          <div className="content">
+            <h2>Portifólio</h2>
+            <div className="imgs">
+              <a href="#" target="_blank"><img src={myPhoto}/></a>
+              <a href="#" target="_blank"><img src={myPhoto}/></a>
+              <a href="#" target="_blank"><img src={myPhoto}/></a>
+              <a href="#" target="_blank"><img src={myPhoto}/></a>
+              <a href="#" target="_blank"><img src={myPhoto}/></a>
+            </div>
+          </div>
         </section>
 
         <section id="contact">
-
+          <p>Quase sempre online, entre em contato :</p>
+          <p>Whatsapp :  <a href="https://wa.me/+5579988105028" target="_blank">(79) 9 8810-5028</a></p>
+          <p>Instagram :  <a href="https://www.instagram.com/antonio_netu/" target="_blank">@antonio_netu</a></p>
+          <p>Email :
+            <a href="https://antonionetodeveloper.github.io/Send-Me-a-Email/" target="_blank"> devantonio.fer@gmail.com
+            </a>
+          </p>
+          <p>Feito por: <a href="https://github.com/antonionetodeveloper" target="_blank">Dev Antônio Fernandes</a></p>
         </section>
 
 
